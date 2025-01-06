8fd408cb874736d7ae50aa3e6cb56889ce6d6e9a
Ansh Anand <anandansh76@gmail.com>
2025-01-06 03:02:30 -0600

    final

--- ubereats_scraper.py
+++ ubereats_scraper.py
@@ -2,9 +2,9 @@ import praw
 
 # Initialize the PRAW client
 reddit = praw.Reddit(
-    client_id='Ccc1KfuiEgX1bOLvw1O9xg',  # Replace with your Client ID
+    client_id='Ccc1KfuiEgX1bOLvw1O9xg', 
     client_secret='ADmSS4D94J6uo-ICoDqj2H1dmNC08g', 
-    user_agent='ubereats scraper by anshlovescoffee',  # A descriptive user agent (e.g., "RedditScraperBot/0.1 by your_username")
+    user_agent='ubereats scraper by anshlovescoffee', 
 )
 
 url = "https://www.reddit.com/r/UberEATS/comments/1hrqgh5/monthly_existing_user_promo_code_thread/"
